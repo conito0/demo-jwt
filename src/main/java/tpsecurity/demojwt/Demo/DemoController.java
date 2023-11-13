ca42cfcfe4262539abc59168704f35f761fc6a84
Constanza <unoconstanza@gmail.com>
2023-11-13 00:12:05 -0300

    Initial commit

---
+++ src/main/java/tpsecurity/demojwt/Demo/DemoController.java
@@ -0,0 +1,24 @@
+package tpsecurity.demojwt.Demo;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class DemoController {
+    @PostMapping(value = "demoUser")
+    public String welcome()
+    {
+        return "Bienvenido desde el endpoint seguro";
+    }
+
+    @PostMapping(value = "demoAdmin")
+    public String welcomeAdmin()
+    {
+        return "Bienvenido desde el endpoint seguro ADMIN";
+    }
+
+}
